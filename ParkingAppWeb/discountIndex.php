57027ca7c8199a672588b309b20cd872f6ad5bae
mber1991 <mber1991@hotmail.com>
2013-10-04 20:46:05 -0500

    Update discountIndex.php
    Redesigned input site, basic functionality for what (I think) is everything we need.

--- ParkingAppWeb/discountIndex.php
+++ ParkingAppWeb/discountIndex.php
@@ -1,48 +1,124 @@
 <!DOCTYPE html>
-<html>
-<head>
-  <title>UNT Parking Garage Console</title>
-  <link rel="stylesheet" type="text/css" href="parkingServices.css" />
-</head>
-
-<body>
-  <h1>UNT Parking Garage Capacity</h1>
-  <form action="process.php" method="post" name="garage_values" id="garage_values" >
-    Current parking capacity:
-    <select name="capacity">
-      <option value="0">0%</option>
-      <option value="10">10%</option>
-      <option value="20">20%</option>
-      <option value="30">30%</option>
-      <option value="40">40%</option>
-      <option value="50">50%</option>
-      <option value="60">60%</option>
-      <option value="70">70%</option>
-      <option value="80">80%</option>
-      <option value="90">90%</option>
-      <option value="100">100%</option>
-    </select>
-    </br>Current Parking Discount:
-    <select name="discount">
-      <option value="0">0%</option>
-      <option value="10">10%</option>
-      <option value="20">20%</option>
-      <option value="30">30%</option>
-      <option value="40">40%</option>
-      <option value="50">50%</option>
-      <option value="60">60%</option>
-      <option value="70">70%</option>
-      <option value="80">80%</option>
-      <option value="90">90%</option>
-      <option value="100">100%</option>
-    </select>
-    </br>
-    <input type="submit" name="update_values" value="update garage" />
-  </form>
+<!-- This site was created in Webflow. http://www.webflow.com--><!-- Last Published: Sat Oct 05 2013 01:19:31 GMT+0000 (UTC) -->
+<html data-wf-site="524dc72fe439c360410001ad">
+  <head>
+    <meta charset="utf-8">
+    <title>Discount UNT App</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/webflow.css">
+    <link rel="stylesheet" type="text/css" href="css/discountuntapp.css">
+  </head><body>
+  <div class="nav-section">
+    <div class="w-container header">
+      <div class="w-row">
+        <div class="w-col w-col-8">
+          <h1 class="w-hidden-tiny">UNT Discount App Management</h1>
+        </div>
+        <div class="w-col w-col-4">
+          <img class="headerimage" src="images/combo_stacked_one-line_WHTongreen_0.png" width="409" alt="combo_stacked_one-line_WHTongreen_0.png">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div class="w-container">
+      <div class="w-row">
+        <div class="w-col w-col-1 w-clearfix"><a class="w-hidden-tiny button expand" href="#">Expand</a>
+        </div>
+        <div class="w-col w-col-11 discountcolumn">
+          <form action="process.php" name="wf-form-new-discount" method="post" name="discount_values" id="discount_values">
+            <label class="tableheader" for="name">Create New Discount</label>
+            <div class="w-row">
+              <div class="w-col w-col-5">
+                <div class="w-container formsection">
+                  <div class="formlabel">Start Date:</div>
+                  <input class="w-input textfield" type="text" placeholder="MM/DD/YYYY" name="startdate" data-name="startDate" required="required"></input>
+                  <div class="formlabel">End Date:</div>
+                  <input class="w-input" type="text" placeholder="MM/DD/YYYY" name="enddate" data-name="endDate" required="required"></input>
+                </div>
+              </div>
+              <div class="w-col w-col-2"></div>
+              <div class="w-col w-col-5">
+                <div class="w-container formsection">
+                  <div class="formlabel">Start Time:</div>
+                  <input class="w-input textfield" type="text" placeholder="00:00" name="starttime" data-name="startTime" required="required"></input>
+                  <div class="formlabel">End Time:</div>
+                  <input class="w-input" type="text" placeholder="24:00" name="endtime" data-name="endtime" required="required"></input>
+                </div>
+              </div>
+            </div>
+            <br>
+            <br>
+            <br>
+            <div class="w-row">
+              <div class="w-col w-col-5">
+                <div class="w-container formsection">
+                  <div class="formlabel">Discount Value:</div>
+                  <select name="discount">
+                    <option value="0">0%</option>
+                    <option value="10">10%</option>
+                    <option value="20">20%</option>
+                    <option value="30">30%</option>
+                    <option value="40">40%</option>
+                    <option value="50">50%</option>
+                    <option value="60">60%</option>
+                    <option value="70">70%</option>
+                    <option value="80">80%</option>
+                    <option value="90">90%</option>
+                    <option value="100">100%</option>
+                  </select>
+                </div>
+              </div>
+              <div class="w-col w-col-2"></div>
+              <div class="w-col w-col-5">
+                <div class="w-container formsection">
+                  <div class="formlabel">Vendor:</div>
+                  <select name="vendor">
+                    <option value="parking">Parking</option>
+                  </select>
+                  <input class="w-button button submit" type="submit" value="Submit" data-wait="Please wait..."></input>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div class="w-container managecontainer">
+      <h2>Manage Current Discounts</h2>
+      <div class="w-container">
+        <div class="w-container managebox">
+          <div class="w-form">
+            <label class="tableheader" for="name">Value:</label>
+            <div class="w-form-done">
+              <p>Thank you! Your submission has been received!</p>
+            </div>
+            <div class="w-form-fail">
+              <p>Oops! Something went wrong while submitting the form :(</p>
+            </div>
+          </div>
+          <div class="w-container">
+            <p>THIS
+              <br>WILL
+              <br>BE
+              <br>A
+              <br>PRETTY
+              <br>TABLE</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
-
 </html>
-
+<!-- Wasn't sure if we needed this yet so I shoved it in at the bottom for now...
+-Matt
+-->
+<!--
 <?php
 /**
  * User: Jeff Watson
@@ -69,4 +145,5 @@ while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
     // print the array in JSON format
     echo json_encode($row);
 }
-
+*/
+-->
