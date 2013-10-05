0125798abbd16a0ec060f941bc05e2351bc53bae
mber1991 <mber1991@hotmail.com>
2013-10-04 20:46:23 -0500

    Update process.php

--- ParkingAppWeb/process.php
+++ ParkingAppWeb/process.php
@@ -1,11 +1,11 @@
 <?php
-$capacity = $_POST['capacity'];
-$discount = $_POST['discount'];
-$parking_array = array("capacity" => $capacity, "discount" => $discount);
+$date_start = $_POST['startdate'];
+$date_end = $_POST['enddate'];
+$time_start = $_POST['starttime'];
+$time_end = $_POST['endtime'];
+$discount_value = $_POST['discount'];
+$vendor_name = $_POST['vendor'];
+$discount_array = array("startdate" => $date_start, "enddate" => $date_end, "starttime" => $time_start, "endtime" => $time_end, "value" => $discount_value, "vendor" => $vendor_name);
 
-  echo "current capacity is: ". $capacity . "</br>";
-  echo "\n";
-  echo "current discount is: ". $discount . "</br>";
-
-  echo json_encode($parking_array);
+  echo json_encode($discount_array);
 ?>
