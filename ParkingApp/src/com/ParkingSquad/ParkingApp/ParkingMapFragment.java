fed5ec0d070bf61ad0b95329878faba9afdd82bf
Kyle <ksibod@gmail.com>
2013-11-05 22:52:24 -0600

    Added the new icons!  We can decide on Thursday if you guys want a specific one.  The "iphone" one is probably more modular and mobile, but I like the "invisible eagle" one.

--- ParkingApp/src/com/ParkingSquad/ParkingApp/ParkingMapFragment.java
+++ ParkingApp/src/com/ParkingSquad/ParkingApp/ParkingMapFragment.java
@@ -85,6 +85,7 @@ public class ParkingMapFragment extends SupportMapFragment {
 
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = super.onCreateView(inflater, container, savedInstanceState);
