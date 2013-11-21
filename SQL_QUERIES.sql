cf93673a11683ff2af75b7822430ade85d269ea6
JeffWatson <jeff.watson4@gmail.com>
2013-11-21 12:10:52 -0600

    Upload SQL for Hayden

---
+++ SQL_QUERIES.sql
@@ -0,0 +1,5 @@
+CREATE TABLE promotions (id INTEGER PRIMARY KEY, start_date text, stop_date text, start_time text, stop_time text, promotion_name text, value text, lat number, lon number, link text);
+
+SELECT * FROM promotions;
+
+INSERT INTO promotions(null, 'start_date','stop_date','start_time','stop_time','promotion_name','value',lat, lon, 'link');
